ed852279dcc95c3588388d84c2d79a01dbbda650
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2024-01-29 22:45:40 +0200

    updated about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,10 +17,10 @@ const About = () => {
 
         <div className='flex flex-col gap-2'>
         <motion.p variants={fadeIn('up', 'tween', 0.2, 1)} className="mt-8 font-normal md:text-[25px] text-[15px] text-center text-secondary-white">
-          Since our establishment in 1993, <span className="font-extrabold text-white">PL Steel</span> Services has thrived on a steadfast commitment to Customer Service. We go beyond mere transactions; we seek to understand and fulfill <span className="font-extrabold text-white">YOUR</span> needs as our valued customer.
+          Since our establishment in 1993, <span className="font-extrabold text-white">PL Steel</span> Services has thrived on a steadfast commitment to Customer Service. We go beyond mere transactions, we seek to understand and fulfill <span className="font-extrabold text-white">YOUR</span> needs as our valued customer.
         </motion.p>
         <motion.p variants={fadeIn('up', 'tween', 0.2, 1)} className="mt-8 font-normal sm:text-[25px] text-[15px] text-center text-secondary-white">
-        We are not just a steel company; we are your <span className="font-extrabold text-white">strategic partner in success</span>. PL Steel Services is where your vision meets our expertise, creating a synergy that propels your projects to new heights. <span className="font-extrabold text-white">Experience the difference with us</span> – where customer service is not just a promise but the cornerstone of our identity.
+        We are not just a steel company. We are your <span className="font-extrabold text-white">strategic partner in success</span>. PL Steel Services is where your vision meets our expertise, creating a synergy that propels your projects to new heights. <span className="font-extrabold text-white">Experience the difference with us</span> – where customer service is not just a promise but the cornerstone of our identity.
         </motion.p>
         </div>
         
